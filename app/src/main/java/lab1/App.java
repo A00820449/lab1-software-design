dd7b0a894d55f718a4ea5d42fe028bdb7657755e
Miguel Tornero <a00820449@itesm.mx>
2023-05-14 14:31:37 -0600

    updated gitignore

--- app/src/main/java/lab1/App.java
+++ app/src/main/java/lab1/App.java
@@ -9,6 +9,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Greeter().getGreeting("bob"));
+        System.out.println(new Greeter().getGreeting(""));
     }
 }
