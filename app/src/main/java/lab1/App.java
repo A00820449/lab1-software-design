110fd8bf92dac0e6fb5ac802b712d2ac8faef35f
Miguel Tornero <a00820449@itesm.mx>
2023-05-14 19:23:37 -0600

    added console application

--- app/src/main/java/lab1/App.java
+++ app/src/main/java/lab1/App.java
@@ -1,7 +1,37 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String storeOption = "";
+        if (args.length > 0) {
+            storeOption = args[0];
+        }
+
+        PizzaStore factory = null;
+        if (storeOption.equalsIgnoreCase("ny")) {
+            factory = new NYPizzaFactory();
+        }
+        else if (storeOption.equalsIgnoreCase("chicago")) {
+            factory = new ChicagoPizzaFactory();
+        }
+        else if (storeOption.equalsIgnoreCase("california")) {
+            factory = new ChicagoPizzaFactory();
+        }
+        else {
+            System.err.println("Sucursal invalida");
+            return;
+        }
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Escribe tu orden:");
+        String answer = s.nextLine().toLowerCase();
+
+        Pizza p = factory.orderPizza(answer);
+
+        if (p != null) {
+            System.out.println("Listo!");
+        }
     }
 }
